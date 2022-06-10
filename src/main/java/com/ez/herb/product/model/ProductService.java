ca814b2a5bd9557de0cb212fba7ac7db50695fdc
potaroPizza <105497269+potaroPizza@users.noreply.github.com>
2022-06-10 17:55:27 +0900

    22-06-10 pm from ac

--- src/main/java/com/ez/herb/product/model/ProductService.java
+++ src/main/java/com/ez/herb/product/model/ProductService.java
@@ -5,4 +5,5 @@ import java.util.List;
 public interface ProductService {
 	List<ProductVO> selectEventList(String eventName);
 	List<ProductVO> selectByCategory(int categoryNo);
+	ProductVO selectByProductNo(int productNo);
 }
